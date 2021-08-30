56f2a631765c2840005b7c96d9c832d2ce1f82bb
Balázs Barna <barna.balazs66@gmail.com>
2021-08-30 19:05:29 +0200

    Introduced sprite centering

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Drawing;
-using System.Collections.Generic;
 
 namespace SpriteSheetGenerator
 {
@@ -9,32 +9,37 @@ namespace SpriteSheetGenerator
 	{
 		static void Main(string[] args)
 		{
+			const string outputFileName = @"\output.png";
 			Console.WriteLine("Please insert the folder location contaning the images:");
 
 			string imageFolderLocation;
 			imageFolderLocation = Console.ReadLine();
 
+			if (imageFolderLocation[0] == '"')
+				imageFolderLocation = imageFolderLocation.Trim('"');
+
 			if (Directory.Exists(imageFolderLocation))
-			{
-				// TODO: Arrays be faster?
-				List<Bitmap> imageList = new();
+            {
+				// Supported extensions: BMP GIF EXIF JPG PNG TIFF (Exclude output images)
+				string[] supportedExtensions = new[] { "bmp", "gif", "exif", "jpg", "png", "tiff" };
+				string[] containedFiles = Directory.EnumerateFiles(imageFolderLocation, "*.*", SearchOption.TopDirectoryOnly).Where(file => supportedExtensions.Any(x => file.ToLower().EndsWith(x)) && !file.EndsWith(outputFileName)).ToArray();
 
-				int maxWidth = 0;
-				int maxHeight = 0;
-
-				// TODO: Supported extensions: BMP GIF JPEG PNG TIFF
-				string[] containedFiles = Directory.GetFiles(imageFolderLocation, "*.png");
 				if (containedFiles.Length == 0)
-                {
+				{
 					Console.WriteLine("No Files Found");
 					Environment.Exit(0);
 				}
 
-				// Reading files into LIST imageList
-				foreach (string fileLocation in containedFiles)
+				Bitmap[] imageList = new Bitmap[containedFiles.Length];
+
+				int maxWidth = 0;
+				int maxHeight = 0;
+
+				// Reading files
+				for (int i = 0; i < imageList.Length; i++)
 				{
-					Console.WriteLine(fileLocation);
-					Bitmap readBitmap = new(fileLocation);
+					Console.WriteLine(containedFiles[i]);
+					Bitmap readBitmap = new(containedFiles[i]);
 					Console.WriteLine(readBitmap.Width + " : " + readBitmap.Height);
 
 					if (readBitmap.Width > maxWidth)
@@ -43,39 +48,36 @@ namespace SpriteSheetGenerator
 					if (readBitmap.Height > maxHeight)
 						maxHeight = readBitmap.Height;
 
-					imageList.Add(readBitmap);
+					imageList[i] = readBitmap;
 				}
 
 				Console.WriteLine("--maxWidth: " + maxWidth);
 				Console.WriteLine("--maxHeight: " + maxHeight);
-				Console.WriteLine("--length: " + imageList.Count);
+				Console.WriteLine("--length: " + imageList.Length);
 
-				// TODO: Center images
-				Bitmap outputImage = new(maxWidth * containedFiles.Length, maxHeight);
+				// Drawing images
+				Bitmap outputImage = new(maxWidth * imageList.Length, maxHeight);
 				using (Graphics outputGraphics = Graphics.FromImage(outputImage))
-                {
+				{
 					Pen debugPen = new (Color.Red, 2);
-					for (int i = 0; i < imageList.Count; i++)
+					for (int i = 0; i < imageList.Length; i++)
 					{
 						#if DEBUG
 							outputGraphics.DrawLine(debugPen, maxWidth * i, 0, maxWidth * i, maxHeight);
 						#endif
-						outputGraphics.DrawImage(imageList[i], maxWidth * i, 0);
+						outputGraphics.DrawImage(imageList[i], (maxWidth * i) + (maxWidth - imageList[i].Width) / 2, (maxHeight - imageList[i].Height) / 2);
 					}
 				}
 
 				// Output
-				string outputFileName = @"\output.png";
 				if (File.Exists(imageFolderLocation + outputFileName))
-				{
-					Random rand = new();
-					outputFileName = @"\output" + rand.Next() + ".png";
-				}
+					File.Delete(imageFolderLocation + outputFileName);
 
 				outputImage.Save(imageFolderLocation + outputFileName);
 				Console.WriteLine(imageFolderLocation + outputFileName);
-			} else
-			{
+			}
+            else
+            {
 				Console.WriteLine("Incorrect Path");
 			}
 		}
